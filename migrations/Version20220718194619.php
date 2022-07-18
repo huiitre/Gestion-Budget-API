d9da4cc972a19dd64711e55dbc2443a191820d94
huiitre <yanis.deluca@laposte.net>
2022-07-18 21:46:36 +0200

    migrate todo

---
+++ migrations/Version20220718194619.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220718194619 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE todo (id INT AUTO_INCREMENT NOT NULL, todolist_id INT NOT NULL, name VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', is_done TINYINT(1) NOT NULL, percent SMALLINT NOT NULL, INDEX IDX_5A0EB6A0AD16642A (todolist_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE todo ADD CONSTRAINT FK_5A0EB6A0AD16642A FOREIGN KEY (todolist_id) REFERENCES todolist (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE todo');
+    }
+}
